9a6ce3e83109a360b980dde86ed9d8c1565ad3fd
upexcel <rahul.excel2011@gmail.com>
2019-09-03 12:13:52 +0530

    initial commit

---
+++ src/app/modules/core/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+
+const route: Array<Route> = [
+  {
+    path: '',
+    component: DashboardComponent
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(route)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class DashboardRoutingModule { }
